64a5874abd8e07b0a498f96f15cc46cbeec8693f
ghost54770 <ghost54.lol@hotmail.fr>
2021-06-12 19:17:38 +0200

    ajout de document

---
+++ Doc microchip DM164137 Curiosity Development Board/Curiosity Demo Code/Curiosity Demo Code/176x/PIC16F1769_Cur_v355.X/Labs/Lab10_HighEnduranceFlash/HEF.c
@@ -0,0 +1,98 @@
+/**
+  High Endurance Flash Lab Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    HEF.c
+
+  Summary:
+    This is the source file for the HEF lab
+
+  Description:
+    This source file contains the code on how the HEF lab works.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65
+        Device            :  PIC16F1769
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 v1.45
+        MPLAB             :  MPLAB X v4.15
+ */
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+/**
+  Section: Included Files
+ */
+
+#include "../../mcc_generated_files/pin_manager.h"
+#include "../../mcc_generated_files/memory.h"
+#include "../../labs.h"
+
+/**
+  Section: Macro Declaration
+ */
+#define HefAddr    0x07F0      // HEF starting address
+
+/**
+  Section: Local Variable Declarations
+ */
+static uint8_t rotateReg;
+
+/*
+                             Application    
+ */
+void HEF(void) {
+
+    if (labState ==  NOT_RUNNING) {
+        LEDs_SetLow();
+
+        labState = RUNNING;
+    }
+
+    if (labState == RUNNING) {
+        uint16_t writeData = 0x0005;
+        uint16_t Buf[ERASE_FLASH_BLOCKSIZE];
+
+        FLASH_WriteWord(HefAddr, Buf, writeData);
+
+        //Read back value and store to LED display
+        rotateReg = FLASH_ReadWord(HefAddr);
+
+        //Determine which LED will light up
+        //ie. which bit in the register the 1 has rotated to.
+        LED_D4_LAT = rotateReg & 1;
+        LED_D5_LAT = (rotateReg & 2) >> 1;
+        LED_D6_LAT = (rotateReg & 4) >> 2;
+        LED_D7_LAT = (rotateReg & 8) >> 3;
+    }
+
+    if (switchEvent) {
+        labState = NOT_RUNNING;
+    }
+}
+/**
+ End of File
+ */
